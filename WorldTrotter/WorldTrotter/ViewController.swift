cfb33abf01607d2f2d49b8e22fe1166ea20d439a
Cody Mudrack <cmmudrack@milligan.edu>
2019-09-24 09:21:05 -0400

    Added conversion technology

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -15,11 +15,45 @@ class ViewController: UIViewController {
         
     }
 
+    @IBOutlet weak var FahrenheitTextField: UITextField!
     @IBOutlet var celsiusLabel: UILabel!
+    var formatter: NumberFormatter = {
+        let fmt = NumberFormatter()
+        fmt.numberStyle = .decimal
+            fmt.minimumFractionDigits = 0
+            fmt.maximumFractionDigits = 1
+            return fmt
+    }()
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet{
+            if let celsiusValue = celsiusValue{
+                celsiusLabel.text = formatter.string(from: NSNumber(value: celsiusValue.value))
+            } else {
+                celsiusLabel.text = "???"
+            }
+            
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
     @IBAction func fahrenheitFieldChange(_ sender: UITextField) {
+        if let text = sender.text, !text.isEmpty, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            celsiusLabel.text = "???"
+        }
         print("Text = \(sender.text)")
     }
     
-
+    @IBAction func TapDetected(_ sender: UITapGestureRecognizer) {
+        FahrenheitTextField.resignFirstResponder()
+    }
 }
 
